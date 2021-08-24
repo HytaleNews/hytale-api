2953483df10880ee6ace1d079c1578308147ee74
Copystrike <verbraeckennick@gmail.com>
2021-08-24 05:13:10 +0200

    Updated tests

--- __tests__/blogpost.ts
+++ __tests__/blogpost.ts
@@ -2,14 +2,11 @@ import fetch from "node-fetch";
 import { getBlogPosts, getBlogPost, getArchivedBlogPosts } from "../src";
 
 describe("Fetch blogposts", () => {
-  test("Check if able to connect with the Hytale API", async (done) => {
+  // todo make this async. They for some reason dont allow async and done in the newer version..
+  test("Check if able to connect with the Hytale API", async () => {
     await fetch("https://hytale.com/")
       .then((response) => {
         expect(response.ok).toBeTruthy();
-        done();
-      })
-      .catch((error) => {
-        done(error);
       });
   });
 
@@ -23,12 +20,11 @@ describe("Fetch blogposts", () => {
     expect(blogSummary.slug).toEqual("a-sneak-peek-at-hytale-s-user-interface");
   });
 
-  test("Fetching an archived blogpost", async (done) => {
+  test("Fetching an archived blogpost", async () => {
     const year = 2020;
     const month = 1;
     const blogSummary = await getArchivedBlogPosts(year, month);
     expect(blogSummary[0].publishedAt.getMonth() + 1).toEqual(month);
     expect(blogSummary[0].publishedAt.getFullYear()).toEqual(year);
-    done();
   });
 });
